49d38b141876c29a59558a18e22a021ac917d422
Eman <emanendris05@gmail.com>
2026-09-08 08:59:24 +0300

    samesite-none

--- Backend/src/utils/generateToken.js
+++ Backend/src/utils/generateToken.js
@@ -16,7 +16,7 @@ const generateToken = (res, userId) =>{
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
